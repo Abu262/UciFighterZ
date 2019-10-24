c020fa25871264284ea1c63beaf4f762035bf7cd
Jaren Gerdes <gerdesjaren@gmail.com>
2019-10-24 10:39:35 -0700

    Timer

--- Written Warriors/Assets/Scripts/Scenes/StateManager.cs
+++ Written Warriors/Assets/Scripts/Scenes/StateManager.cs
@@ -9,11 +9,16 @@ public class StateManager : MonoBehaviour
 {
     public float currentCountdown;
     public Text timerLabel;
+    public GameObject shakeObj;
+    float speed = 2.0f;
+    float amount = 2.0f;
+    Vector2 startingPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPos.x = gameObject.transform.position.x;
+        startingPos.y = gameObject.transform.position.y;
         StartCoroutine(startCountdown(currentCountdown));
     }
 
@@ -21,10 +26,24 @@ public class StateManager : MonoBehaviour
     {
         while (countdown > 0)
         {
+            StartCoroutine(shake(countdown));
             Debug.Log("Countdown: " + countdown);
             timerLabel.text = (countdown).ToString("0");
             yield return new WaitForSeconds(1.0f);
             countdown--;
+        }
+    }
+
+    IEnumerator shake(float countdown)
+    {
+        if (countdown % 15 == 0)
+        {
+            for (int x = 0; x < 30; x++)
+            {
+                Vector3 v = new Vector3(startingPos.x + (Mathf.Sin(Time.time * speed) * amount), startingPos.y + (Mathf.Sin(Time.time * speed) * amount));
+                gameObject.transform.position = v;
+            }
+            yield return null;
         }
     }
 }
