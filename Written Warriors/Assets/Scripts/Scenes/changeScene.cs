b809ed881d6a926932b2536da611e743c1ef0cbe
Jaren Gerdes <gerdesjaren@gmail.com>
2019-11-05 16:44:53 -0800

    Updated How2Play

---
+++ Written Warriors/Assets/Scripts/Scenes/changeScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeScene : MonoBehaviour
+{
+    public void sceneChange(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
