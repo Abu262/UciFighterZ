c7b2845c402c4e5079337d777093a60d343b3d31
Jesse <jesseallas@gmail.com>
2020-02-24 16:50:48 -0800

    Anims work

--- Written Warriors/Assets/Resources/Wong Ma/WongMa.cs
+++ Written Warriors/Assets/Resources/Wong Ma/WongMa.cs
@@ -26,7 +26,7 @@ public class WongMa : Character
         Player P = SpecHitBox.GetComponent<Player>();
  //       Vector2 constMotion = new Vector2(-1.0f * MoveSpeed * speedscalar * P.transform.localScale.x, 0.0f);
         Vector3 s = P.transform.localScale;
-        P.Aura.SetActive(true);
+        //P.Aura.SetActive(true);
         int F = SpecAtkHit;
 
         while (R > 0)
@@ -35,39 +35,52 @@ public class WongMa : Character
             {
                 if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKey(KeyCode.Alpha4))
                 {
+                    P.animator.SetBool("IsMedFeint", true);
+                    P.animator.SetBool("IsSpecAtk", false);
+
                     F = MedAtkStartUp;
                     while (F > 0)
                     {
-                        Debug.Log("MEDIUM");
+                        //Debug.Log("MEDIUM");
                         P.transform.localScale = s;
                         F -= 1;
                         yield return null;
                     }
+                    R = 0;
                 }
                 //X
                 if (Input.GetKeyDown(KeyCode.Joystick1Button1) || Input.GetKey(KeyCode.R))
                 {
+                    P.animator.SetBool("IsLowFeint", true);
+                    P.animator.SetBool("IsSpecAtk", false);
+
                     F = LowAtkStartUp;
                     while (F > 0)
                     {
-                        Debug.Log("LOW");
+                       // Debug.Log("LOW");
                         P.transform.localScale = s;
                         F -= 1;
                         yield return null;
                     }
+                    R = 0;
                 }
                 //triangle
                 if (Input.GetKeyDown(KeyCode.Joystick1Button3) || Input.GetKey(KeyCode.Alpha5))
                 {
+                    P.animator.SetBool("IsHighFeint", true);
+                    P.animator.SetBool("IsSpecAtk", false);
+
                     F = HighAtkStartUp;
                     while (F > 0)
                     {
-                        Debug.Log("HIGH");
+                       //Debug.Log("HIGH");
                         P.transform.localScale = s;
                         F -= 1;
                         yield return null;
                     }
+                    R = 0;
                 }
+
 
 
 
@@ -77,38 +90,50 @@ public class WongMa : Character
             {
                 if (Input.GetKeyDown(KeyCode.Joystick2Button0) || Input.GetKey(KeyCode.Alpha4))
                 {
+                    P.animator.SetBool("IsMedFeint", true);
+                    P.animator.SetBool("IsSpecAtk", false);
+
                     F = MedAtkStartUp;
                     while (F > 0)
                     {
-                        Debug.Log("MEDIUM");
+                        //Debug.Log("MEDIUM");
                         P.transform.localScale = s;
                         F -= 1;
                         yield return null;
                     }
+                    R = 0;
                 }
                 //X
                 if (Input.GetKeyDown(KeyCode.Joystick2Button1) || Input.GetKey(KeyCode.R))
                 {
+                    P.animator.SetBool("IsLowFeint", true);
+                    P.animator.SetBool("IsSpecAtk", false);
+                    
                     F = LowAtkStartUp;
                     while (F > 0)
                     {
-                        Debug.Log("LOW");
+                        //Debug.Log("LOW");
                         P.transform.localScale = s;
                         F -= 1;
                         yield return null;
                     }
+                    R = 0;
                 }
                 //triangle
                 if (Input.GetKeyDown(KeyCode.Joystick2Button3) || Input.GetKey(KeyCode.Alpha5))
                 {
+                    P.animator.SetBool("IsHighFeint", true);
+                    P.animator.SetBool("IsSpecAtk", false);
+
                     F = HighAtkStartUp;
                     while (F > 0)
                     {
-                        Debug.Log("HIGH");
+                        //Debug.Log("HIGH");
                         P.transform.localScale = s;
                         F -= 1;
                         yield return null;
                     }
+                    R = 0;
                 }
 
 
@@ -118,18 +143,23 @@ public class WongMa : Character
             yield return null;
         }
 
+        P.animator.SetBool("IsSpecAtk", false);
+        P.animator.SetBool("IsCrouch", false);
+        P.animator.SetBool("IsStanding", true);
+        P.animator.SetBool("IsMedFeint", false);
+        P.animator.SetBool("IsLowFeint", false);
+        P.animator.SetBool("IsHighFeint", false);
 
 
-        
 
 
 
 
-       // SpecHitBox.enabled = false;
+        // SpecHitBox.enabled = false;
         //P.HighBlocking = false;
         //P.LowBlocking = false;
 
-        P.Aura.SetActive(false);
+        //P.Aura.SetActive(false);
 
 
         yield return null;
